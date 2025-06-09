c8401b2df782aa707a715ef795179fcbfc1c22da
Frederick Lawson <fredjlawson@gmail.com>
2025-06-09 12:26:56 -0300

    minor

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -35,6 +35,7 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/moorings') &&
     request.nextUrl.pathname !== '/' // Add this line
   ) {
     // no user, potentially respond by redirecting the user to the login page
