9e243c1a158570911c452fdf0ed1b0b307feed2f
Frederick Lawson <freddie@corneliajames.com>
2025-06-10 07:54:06 -0300

    messaging

--- src/actions/messages.ts
+++ src/actions/messages.ts
@@ -3,19 +3,17 @@ import type { Mooring } from '@/types/mooring';
 import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 import { User } from '@supabase/supabase-js';
-import { createClient } from '@/lib/supabase/server';
+import { adminAuthClient } from '@/lib/supabase/authClient';
 
 export async function sendMessage({ mooring, user, message }: { mooring: Mooring; user: User; message: string }) {
   const resend = new Resend(process.env.RESEND_API_KEY);
-  const supabase = await createClient();
-  console.log('mooring owner_id', mooring.owner_id);
-  const { data: ownerData } = await supabase.auth.admin.getUserById(mooring.owner_id);
-  console.log('ownerData', ownerData);
+  const { data: ownerData } = await adminAuthClient.getUserById(mooring.owner_id);
   const { data, error } = await resend.emails.send({
-    from: user.email!,
+    from: 'HeyBuoy <inquiry@heybuoy.beam.bm>',
     to: ownerData.user?.email!,
     subject: 'Message from ' + user.user_metadata.first_name! + ' ' + user.user_metadata.last_name!,
     react: EmailTemplate({ sender: user, message }) as React.ReactNode,
+    replyTo: user.email!,
   });
   if (error) {
     console.error('Error sending message:', error);
