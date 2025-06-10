871bb6170b9647e012bc934fbd82af0fceeb473c
Frederick Lawson <freddie@corneliajames.com>
2025-06-10 10:59:02 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
-import Header from '@/components/header';
+
 import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from '@/components/ui/sonner';
 import { getUserServer } from '@/lib/utils/get-user-server';
 import Link from 'next/link';
+import { HeaderNav } from '@/components/header-nav';
 
 export const metadata = {
   title: 'Hey Buoy',
@@ -24,6 +25,20 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
+  );
+}
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between border-b bg-white p-6">
+      <div className="mr-4 flex">
+        <Link href="/" className="mr-6 flex flex-col">
+          <span className="text-2xl font-bold">Hey Buoy</span>
+          <span className="text-muted-foreground text-sm">The home of Bermuda Moorings</span>
+        </Link>
+      </div>
+      <HeaderNav />
+    </header>
   );
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,13 @@
+import { User as SupabaseUser } from '@supabase/supabase-js';
 import Link from 'next/link';
-import { createClient } from '@/lib/supabase/server';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 
-import { User as SupabaseUser } from '@supabase/supabase-js';
-
+import { HeaderNav } from '@/components/header-nav';
 import CreateMooring from './main/create-mooring';
 import CreateRequest from './main/create-request';
 import { Button } from './ui/button';
 
 export default async function Header() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <header className="flex items-center justify-between border-b bg-white p-6">
       <div className="mr-4 flex">
@@ -22,10 +16,7 @@ export default async function Header() {
           <span className="text-muted-foreground text-sm">The home of Bermuda Moorings</span>
         </Link>
       </div>
-      <div className="items-top flex flex-1 flex-col justify-end gap-2 md:flex-row">
-        <UserMenu user={user} />
-        <GenericMenu user={user} />
-      </div>
+      <HeaderNav />
     </header>
   );
 }
