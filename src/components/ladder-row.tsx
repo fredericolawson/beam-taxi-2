8fa9f1cd3a615b8ac819fe6c6a3c06b0d42631e3
Frederick Lawson <fredjlawson@gmail.com>
2025-06-27 17:34:10 -0300

    fix

--- src/components/ladder-row.tsx
+++ src/components/ladder-row.tsx
@@ -32,7 +32,7 @@ export function LadderRow({ player, currentPlayer, history }: { player: Player; 
           <MatchHistorySummary historySummary={history.summary} />
         </TableCell>
         <TableCell className="w-32">
-          {history.matches[0].completedOn ? new Date(history.matches[0].completedOn).toLocaleDateString() : '—'}
+          {history.matches[0]?.completedOn ? new Date(history.matches[0].completedOn).toLocaleDateString('en-gb') : '—'}
         </TableCell>
         <TableCell className="w-32">
           <LadderAction player={player} currentPlayer={currentPlayer} />
