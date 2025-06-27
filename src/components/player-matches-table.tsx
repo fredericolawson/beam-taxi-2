5314cb92f2c77b9235937a305eb9bab33186dc50
Frederick Lawson <fredjlawson@gmail.com>
2025-06-27 17:50:13 -0300

    minor

--- src/components/player-matches-table.tsx
+++ src/components/player-matches-table.tsx
@@ -6,7 +6,7 @@ export function PlayerMatchesTable({ matches, player }: { matches: CompletedMatc
   if (!matches) return null;
   return (
     <div>
-      <h2 className="text-center font-semibold">Match History</h2>
+      <h2 className="mb-2 text-center font-semibold">Match History</h2>
       <Table className="rounded-md border bg-white">
         <TableHeader>
           <MatchHeader />
