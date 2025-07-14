54b4de960ca3fef0b35b39dcbfb24a652891c5ad
Frederick Lawson <fredjlawson@gmail.com>
2025-07-14 13:49:30 -0300

    list trips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
 import { getUserServer } from '@/lib/utils/get-user-server';
 import { getActiveTrips } from '@/actions/trip';
 import { TripCard } from '@/components/trip-card';
+import { listTripsByRiderId } from '@/lib/tables/trips';
+import { Trip } from '@/types';
 
 export default async function Home() {
   const user = await getUserServer();
-  const activeTrips = await getActiveTrips({ riderId: user!.id });
+  const trips = await listTripsByRiderId({ riderId: user!.id });
+  const assignedTrips = trips.filter((trip) => trip.status === 'assigned');
+  const pendingTrips = trips.filter((trip) => trip.status === 'pending');
 
   return (
     <div className="flex w-full flex-col space-y-6">
-      <h2 className="heading-2">Active Requests</h2>
+      <TripList trips={assignedTrips} title="Assigned Trips" />
+      <TripList trips={pendingTrips} title="Pending Trips" />
+    </div>
+  );
+}
+function TripList({ trips, title }: { trips: Trip[]; title: string }) {
+  return (
+    <div className="flex w-full flex-col space-y-6">
+      <h2 className="heading-2">{title}</h2>
 
-      {activeTrips.length === 0 ? (
+      {trips.length === 0 ? (
         <p className="text-muted-foreground">No active trips</p>
       ) : (
         <div className="space-y-4">
-          {activeTrips.map((trip) => (
+          {trips.map((trip) => (
             <TripCard key={trip.id} trip={trip} />
           ))}
         </div>
